8032ca95caf0506c0f49fe7b66e48e05cd0cee26
Simoso <87707341+Simoso68@users.noreply.github.com>
2023-09-04 15:22:03 +0200

    fix

--- src/main.py
+++ src/main.py
@@ -1,27 +1,38 @@
-from sys import argv
+from sys import argv, exit
 from read import readfile
 from hash import *
 
 VERSION = "1.0.0"
-print_version = False
-print_help = False
 files = []
 
-if argv[1] == "sha1" or argv[1] == "sha256" or argv[1] == "sha512" or argv[1] == "md5":
-    hashtype = argv[1]
-else:
-    print(f"error: unknown hashing algorithm: {argv[1]}")
-    exit()
+try:
+    if argv[1] == "sha1" or argv[1] == "sha256" or argv[1] == "sha512" or argv[1] == "md5":
+        hashtype = argv[1]
+    else:
+        print(f"error: unknown hashing algorithm: {argv[1]}")
+        exit()
+except IndexError:
+    print(f"error: not enough arguments.")
+    exit(1)
 
 for arg in argv[2:]:
     if arg.startswith("-"):
         if arg == "-v" or arg == "--version":
-            print_version = True
+            print(f"getfilehash by Simoso68 running on version {VERSION}, licensed under GNU GPL 3.")
+            exit(1)
         elif arg == "-h" or arg == "--help":
-            print_help = True
+            print("""
+General usage:
+    <hash-algorithm> <files>
+Arguments:
+    -v / --version              Output version of getfilehash
+    -h / --help                 Help for getfilehash
+Example:
+    sha256 file1.o file2.o""".replace("\n", "", 1))
+            exit(0)
         else:
             print(f"error: unknown argument: {arg}")
-            exit()
+            exit(1)
     else:
         files.append(arg)
 
@@ -30,15 +41,3 @@ for file in files:
     elif hashtype == "sha256": print(getsha256(readfile(file)))
     elif hashtype == "sha512": print(getsha512(readfile(file)))
     elif hashtype == "md5": print(getmd5(readfile(file)))
-
-if print_version:
-    print(f"getfilehash by Simoso68 running on version {VERSION}, licensed under GNU GPL 3.")
-if print_help:
-    print("""
-General usage:
-    <hash-algorithm> <files>
-Arguments:
-    -v / --version              Output version of getfilehash
-    -h / --help                 Help for getfilehash
-Example:
-    sha256 file1.o file2.o""".replace("\n", "", 1))
